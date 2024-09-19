612de3da9c0adf44f599f6c236fc25dd7336ecde
IannisG10 <guerraiannis@gmail.com>
2024-09-19 12:11:57 +0200

    Ajout d'une fonction pour tourner le dé

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-from random import random
+import tkinter as tk
+import random
 
-print("Hello DiceUp")
+
+def lancer_de():
+    r = random.randint(1,6)
+    label.config(text=str(r))
+
+
+window = tk.Tk()
+window.title("DiceUp")
+
+label = tk.Label(window,text="1",font=("Arial",20))
+label.pack(pady=10)
+
+button = tk.Button(window,text="Tourne le dé",command=lancer_de)
+button.pack(pady=30)
+
+window.mainloop()
+
+
+
+
